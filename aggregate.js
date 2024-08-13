6b0007050e1cfbb5313492c5fb02e17580810f95
Daniil Kirichenko <daniil@kirick.me>
2024-08-13 19:24:31 +0300

    Update aggregate.js

--- aggregate.js
+++ aggregate.js
@@ -1,5 +1,5 @@
 
-const { appendFileSync } = require('node:fs');
+import { appendFile } from 'node:fs/promises';
 
 const files = { A: 0, M: 0, D: 0 };
 for (const op of process.env.FILES.split(/\s/).filter(a => a.length > 0)) {
@@ -12,7 +12,7 @@ for (const op of process.env.FILES.split(/\s/).filter(a => a.length > 0)) {
 const [ , lines_added = 0 ] = process.env.LINES.match(/\s+(\d+)\s+insertions/) ?? [];
 const [ , lines_deleted = 0 ] = process.env.LINES.match(/\s+(\d+)\s+deletions/) ?? [];
 
-appendFileSync(
+await appendFile(
 	process.env.GITHUB_OUTPUT,
 	'value=' + JSON.stringify({
 		repo_name: process.env.GITHUB_REPOSITORY,
