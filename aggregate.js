cc62ffaa4b63cb36aa17451ca65229f7837dd094
Daniil Kirichenko <daniil@kirick.me>
2024-09-04 12:17:51 +0300

    Update aggregate.js

--- aggregate.js
+++ aggregate.js
@@ -1,16 +1,17 @@
-
-import { appendFile } from 'node:fs/promises';
+import { $ } from 'bun';
 
 const files = { A: 0, M: 0, D: 0 };
-for (const op of process.env.FILES.split(/\s/).filter(a => a.length > 0)) {
-	try {
-		files[op]++;
+for await (const line of $`git diff --name-status HEAD~1 HEAD`.lines()) {
+	const [ status ] = line.split(/\s+/);
+
+	if (status in files) {
+		files[status]++;
 	}
-	catch {}
 }
 
-const [ , lines_added = 0 ] = process.env.LINES.match(/\s+(\d+)\s+insertions/) ?? [];
-const [ , lines_deleted = 0 ] = process.env.LINES.match(/\s+(\d+)\s+deletions/) ?? [];
+const LINES = await $`git diff --shortstat HEAD~1 HEAD`.text();
+const [ , lines_added = 0 ] = LINES.match(/(?:^|\s)(\d+)\s+insertions/) ?? [];
+const [ , lines_deleted = 0 ] = LINES.match(/(?:^|\s)(\d+)\s+deletions/) ?? [];
 
 await appendFile(
 	process.env.GITHUB_OUTPUT,
