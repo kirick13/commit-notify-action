20f3b8bf8036e05f21eeaa585d79c3d91141a3d9
Daniil Kirichenko <daniil@kirick.me>
2024-09-04 12:45:17 +0300

    Update send-telegram.js

--- send-telegram.js
+++ send-telegram.js
@@ -1,26 +1,26 @@
 const { repo_name, files, lines } = JSON.parse(process.env.DATA);
 
 const text_lines = [
-	`👨‍💻 New commit to *${repo_name}*:`,
+	`👨‍💻 New commit to <b>${repo_name}</b>:`,
 ];
 
 if (files.A > 0) {
-	text_lines.push(`\u00a0→\u00a0\u00a0*${files.A}* new file${files.A === 1 ? '' : 's'} created`);
+	text_lines.push(`\u00a0→\u00a0\u00a0<b>${files.A}</b> new file${files.A === 1 ? '' : 's'} created`);
 }
 if (files.M > 0) {
-	text_lines.push(`\u00a0→\u00a0\u00a0*${files.M}* file${files.M === 1 ? '' : 's'} modified`);
+	text_lines.push(`\u00a0→\u00a0\u00a0<b>${files.M}</b> file${files.M === 1 ? '' : 's'} modified`);
 }
 if (files.D > 0) {
-	text_lines.push(`\u00a0→\u00a0\u00a0*${files.D}* file${files.D === 1 ? '' : 's'} deleted`);
+	text_lines.push(`\u00a0→\u00a0\u00a0<b>${files.D}</b> file${files.D === 1 ? '' : 's'} deleted`);
 }
 
 text_lines.push(
-	`\u00a0*\uFF0B*\u00a0\u00a0*${lines.added}* line${lines.added === 1 ? '' : 's'} of code added`,
-	`\u00a0*\uFF0D*\u00a0\u00a0*${lines.deleted}* line${lines.deleted === 1 ? '' : 's'} of code deleted`,
+	`\u00a0<b>\uFF0B</b>\u00a0\u00a0<b>${lines.added}</b> line${lines.added === 1 ? '' : 's'} of code added`,
+	`\u00a0<b>\uFF0D</b>\u00a0\u00a0<b>${lines.deleted}</b> line${lines.deleted === 1 ? '' : 's'} of code deleted`,
 );
 
 if (process.env.SIGNATURE) {
-	text_lines.push(`>${process.env.SIGNATURE}`);
+	text_lines.push(`<blockquote>${process.env.SIGNATURE}</blockquote>`);
 }
 
 console.log(text_lines.join('\n'));
@@ -36,7 +36,7 @@ const response = await fetch(
 		body: JSON.stringify({
 			chat_id: process.env.TELEGRAM_CHAT_ID,
 			text: text_lines.join('\n'),
-			parse_mode: 'markdown',
+			parse_mode: 'html',
 		}),
 	},
 );
