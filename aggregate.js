19880f04fef9cbc0c353dacaaa89a79a7f305cfb
Daniil Kirichenko <daniil@kirick.me>
2025-01-23 22:42:12 +0300

    Update aggregate.js

--- aggregate.js
+++ aggregate.js
@@ -1,8 +1,8 @@
 import { $ }          from 'bun';
 import { appendFile } from 'node:fs/promises';
 
-const repo_name = process.env.GITHUB_REPOSITORY.endsWith('.git')
-	? new URL(process.env.GITHUB_REPOSITORY).pathname.replace(/^\//, '').replace(/\.git$/, '');
+const repo_name = process.env.GITHUB_REPOSITORY.match(/^https?:/) && process.env.GITHUB_REPOSITORY.endsWith('.git')
+	? new URL(process.env.GITHUB_REPOSITORY).pathname.replace(/^\//, '').replace(/\.git$/, '')
 	: process.env.GITHUB_REPOSITORY;
 
 const files = { A: 0, M: 0, D: 0 };
